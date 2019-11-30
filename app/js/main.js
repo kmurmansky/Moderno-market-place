119f771d2e53ff014b5a72c9b07a876435bb1ba3
kmurmansky <kmurmansky@gmail.com>
2019-11-30 14:09:07 +0300

    added breadcrumbs

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,28 @@
 $(function(){
 
-  $(".rate-star").rateYo({
+  $(".rate-star").rateYo({ // Рейтинг, статичный
     rating: 5,
     starWidth: "12px",
     readOnly: true,
   });
 
-  $('.product-slider__inner').slick({
+  $('.product-slider__inner').slick({ // Слайдер с точками, без стрелок
     slidesToShow: 4, 
 		slidesToScroll: 4,
     dots: true,
     arrows: false,
     dotsClass: 'slider__dots',
-
   });
+
+  $(".js-range-slider").ionRangeSlider({ // Слайдер, диапазон цен
+    type: "double",
+    skin: "round",
+    min: 0,
+    max: 1999,
+    from: 0,
+    to: 600,
+    prefix: "$",
+});
 
   var mixer = mixitup('.products__inner-box');
 
