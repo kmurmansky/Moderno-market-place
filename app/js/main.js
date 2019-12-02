2e3d3ec6fbb4fb2990eb70fd3f9e65eb8b0b1326
kmurmansky <kmurmansky@gmail.com>
2019-12-02 05:00:22 +0300

    added section products-page__content

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
 $(function(){
 
-  $(".rate-star").rateYo({ // Рейтинг, статичный
+  $(".rate-star").rateYo({                       // Рейтинг, статичный - rateYo
     rating: 5,
     starWidth: "12px",
     readOnly: true,
   });
 
-  $('.product-slider__inner').slick({ // Слайдер с точками, без стрелок
+  $('.product-slider__inner').slick({            // Слайдер с точками, без стрелок - slick
     slidesToShow: 4, 
 		slidesToScroll: 4,
     dots: true,
@@ -14,7 +14,7 @@ $(function(){
     dotsClass: 'slider__dots',
   });
 
-  $(".js-range-slider").ionRangeSlider({ // Слайдер, диапазон цен
+  $(".js-range-slider").ionRangeSlider({        // Слайдер, диапазон цен - ionRangeSlider, с кастомным скином
     type: "double",
     skin: "round",
     min: 0,
@@ -24,6 +24,23 @@ $(function(){
     prefix: "$",
 });
 
-  var mixer = mixitup('.products__inner-box');
+                                  
+$('.icon-th-list').on('click', function(){      // 1. Порядок расположения айтемов, сетка или список, при нажатии на кнопку смены вида
+  $('.products__item').addClass('list');      
+
+  $('.icon-th-list').addClass('active');        // 2. Изменение цвета у активной кнопки.
+  $('.icon-th-large').removeClass('active');
+
+});
+
+$('.icon-th-large').on('click', function(){     // 1^
+  $('.products__item').removeClass('list');
+
+  $('.icon-th-large').addClass('active');       // 2^
+  $('.icon-th-list').removeClass('active'); 
+});
+// 
+
+var mixer = mixitup('.products__inner-box');  // Фильтрация, mixitup
 
 });
