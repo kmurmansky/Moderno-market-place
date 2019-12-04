142c5fa5a352026f8927f140a1d3e687e123e49f
kmurmansky <kmurmansky@gmail.com>
2019-12-04 15:26:44 +0300

    adaptive header&footer

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,18 @@ $('.icon-th-large').on('click', function(){     // 1^
 });
 // 
 
+// .menu__btn
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+
+// .header__btn-menu
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
+
+
 var mixer = mixitup('.products__inner-box');  // Фильтрация, mixitup
 
 });
